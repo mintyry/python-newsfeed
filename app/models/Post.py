242cc3874caa642ba22df5f1bde2015b78c7d06c
mintyry <gitmintyry@gmail.com>
2024-04-03 18:06:55 -0700

    comment model complete

--- app/models/Post.py
+++ app/models/Post.py
@@ -3,8 +3,6 @@ from app.db import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
-user = relationship('User')
-
 # making post model
 class Post(Base):
     __tablename__ = 'posts'
@@ -14,3 +12,6 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    user = relationship('User')
+    comments = relationship('Comment', cascade='all,delete')
