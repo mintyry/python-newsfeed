f0eec9e63b5684018c6c68c303aad27ed4212732
mintyry <gitmintyry@gmail.com>
2024-04-09 20:19:35 -0700

    error handling with try...except

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,17 +10,24 @@ def signup():
     data = request.get_json()
     db = get_db()
 
+    try:
+
 # creates new user
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    # save in database
-    # preps the INSERT statement
-    db.add(newUser)
-    # updates database
-    db.commit()
-
+        # save in database
+        # preps the INSERT statement
+        db.add(newUser)
+        # updates database
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+        # insert failed, so rollback the commit and send error to front end
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
+    
     return jsonify(id = newUser.id)
