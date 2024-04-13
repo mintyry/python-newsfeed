92165b047a6ddd4b6a03f96256d855fdfa22dfd0
mintyry <gitmintyry@gmail.com>
2024-04-12 21:24:23 -0700

    updated dashboard dash() to render posts on dashboard

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,12 +1,23 @@
 from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
 
 # url_prefix prefixes every route with /dashboard; thus: /dashboard/edit/<id>
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
+    db = get_db()
+    posts = (
+        db.query(Post)
+        .filter(Post.user_id == session.get('user_id'))
+        .order_by(Post.created_at.desc())
+        .all()
+    )
+
     return render_template(
         'dashboard.html',
+        posts = posts,
         loggedIn = session.get('loggedIn')
     )
 
