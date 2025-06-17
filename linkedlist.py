ec986a6fd9e0451e8bad287dba9c224be8e51402
mintyry <gitmintyry@gmail.com>
2025-06-16 20:00:04 -0700

    created linkedlist linkedlist class

--- linkedlist.py
+++ linkedlist.py
@@ -17,3 +17,50 @@ class Node(object):
         return self.data
     def set_data (self, d):
         self.data = d
+ # write linkedlist class
+ # every linkedlist needs to have two instance variables: root and size
+class LinkedList (object):
+    def __init__(self, r = None):
+        self.root = r
+        self.size = 0
+    def get_size (self):
+        return self.size
+    def add (self, d):
+        new_node = Node (d, self.root)
+        self.root = new_node
+        self.size += 1
+    def remove (self, d):
+        # track node we're looking at, starting at root
+        this_node = self.root
+        # track previous node because we're going to have to change pointer of previous node
+        prev_node = None
+        # iterate through list and if we find node we lookin for, and if we're not in root node, then set prev node's next node pointer to this node's next node pointer
+        while this_node:
+            if this_node.get_data() == d:
+                if prev_node:
+                    prev_node.set_next(this_node.get_next())
+                else:
+                    # if we are in the root, then there' no prev node
+                    self.root = this_node
+                    self.size -= 1
+                    # indicates we successfully removed the data
+                    return True
+           #if we didnt find data in current node, then advance pointer from prev node to next node and this node to next node
+            else:
+                prev_node = this_node
+                this_node = this_node.get_next()
+        # exit while loop and we didnt successfully remove the data:
+        return False
+    def find (self, d):
+        # start at root
+        this_node = self.root
+        # while theres another node, look at each node to compare data against data we're looking for
+        while this_node:
+            if this_node.get_data() == d:
+            # finds it, return data
+                return d
+        else:
+            # doesnt find it, move on
+            this_node = this_node.get_next()
+        # if exits loop without finding data, return non
+        return None
